738ef4adcc61914dc5cfb2f152b295dec29aa0c2
Angela <angelaranola30@gmail.com>
2018-12-04 22:43:03 -0700

    New version of final project

---
+++ finalProject/js/drag.js
@@ -0,0 +1,36 @@
+$(function() {
+
+    $(".draggableClone").draggable({
+        revert: 'invalid'
+    });
+
+    $(".draggable").draggable({
+        revert: 'invalid',
+        helper: 'clone',
+        scroll: false
+    });
+
+    $("#droppable").droppable({
+        accept: ".draggable",
+        drop: function (event, ui) {
+            $(this).append($(ui.helper).clone().removeClass("draggable").addClass("draggableClone").draggable({
+                helper: "original",
+                containment: "parent"
+            }));
+        }
+    });
+
+    $("#trash").droppable({
+        accept: ".draggableClone",
+        drop: function(event, ui) {
+            ui.draggable.remove();
+        }
+    });
+});
+
+
+// $('#trash').droppable({
+//     over: function(event, ui) {
+//         ui.draggable.remove();
+//     }
+// });
