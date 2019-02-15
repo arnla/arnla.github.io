2f58579f7e8325b3271c695a44a8935d179de830
Angela <angelaranola30@gmail.com>
2019-02-15 16:37:34 -0700

    Layout good. Need to fix logic

--- seng513/a2/calculator.js
+++ seng513/a2/calculator.js
@@ -1,117 +1,135 @@
 $(document).ready(function() {
+    setHeight();
+    changeFontSize();
+    let result;
+    let expression;
+    let evaluated = true;
     $('button').click(function() {
-        let exp = $('#evaluateText').val();
-        if (evalState === 'evaluated') {
+        let exp = $('#expression').text();
+        if (exp === '0') {
             exp = '';
-            $('#evaluateText').val(exp);
-            evalState = 'new';
         }
         switch (this.id) {
             case 'btn0':
-                $('#evaluateText').val(exp + '0');
+                $('#expression').text(exp + '0');
                 break;
             case 'btn1':
-                $('#evaluateText').val(exp + '1');
+                $('#expression').text(exp + '1');
                 break;
             case 'btn2':
-                $('#evaluateText').val(exp + '2');
+                $('#expression').text(exp + '2');
                 break;
             case 'btn3':
-                $('#evaluateText').val(exp + '3');
+                $('#expression').text(exp + '3');
                 break;
             case 'btn4':
-                $('#evaluateText').val(exp + '4');
+                $('#expression').text(exp + '4');
                 break;
             case 'btn5':
-                $('#evaluateText').val(exp + '5');
+                $('#expression').text(exp + '5');
                 break;
             case 'btn6':
-                $('#evaluateText').val(exp + '6');
+                $('#expression').text(exp + '6');
                 break;
             case 'btn7':
-                $('#evaluateText').val(exp + '7');
+                $('#expression').text(exp + '7');
                 break;
             case 'btn8':
-                $('#evaluateText').val(exp + '8');
+                $('#expression').text(exp + '8');
                 break;
             case 'btn9':
-                $('#evaluateText').val(exp + '9');
+                $('#expression').text(exp + '9');
                 break;
             case 'btnLeftBracket':
                 if (isBracketValid('(',exp)) {
-                    $('#evaluateText').val(exp + '(');
+                    $('#expression').text(exp + '(');
                     openBrackets = true;
                 }
                 break
             case 'btnRightBracket':
                 if (isBracketValid(')',exp)) {
-                    $('#evaluateText').val(exp + ')');
+                    $('#expression').text(exp + ')');
                     openBrackets = false;
                 }
                 break;
             case 'btnClear':
-                $('#evaluateText').val('');
+                $('#result').text(result);
+                $('#expression').text('0');
                 break;
             case 'btnDelete':
-                $('#evaluateText').val(exp.substr(0, exp.length - 1));
+                if ($('#expression').text() !== '0') {
+                    $('#expression').text(exp.substr(0, exp.length - 1));
+                }
                 break;
             case 'btnSubtract':
                 if (isOperatorValid(exp)) {
-                    $('#evaluateText').val(exp + '-');
+                    $('#expression').text(exp + '-');
                 }
                 break;
             case 'btnAdd':
                 if (isOperatorValid(exp)) {
-                    $('#evaluateText').val(exp + '+');
+                    $('#expression').text(exp + '+');
                 }
                 break;
             case 'btnMultiply':
                 if (isOperatorValid(exp)) {
-                    $('#evaluateText').val(exp + '*');
+                    $('#expression').text(exp + '*');
                 }
                 break;
             case 'btnDivide':
                 if (isOperatorValid(exp)) {
-                    $('#evaluateText').val(exp + '/');
+                    $('#expression').text(exp + '/');
                 }
                 break;
             case 'btnDecimal':
-                $('#evaluateText').val(exp + '.');
+                $('#expression').text(exp + '.');
                 break;
             case 'btnEvaluate':
                 try {
-                    $('#evaluateText').val(eval(exp));
+                    $('#result').text(exp + ' =');
+                    $('#expression').text(eval(exp));
                 } catch(e) {
-                    $('#evaluateText').val('ERROR');
+                    $('#expression').text('ERROR');
                 }
-                evalState = 'evaluated';
                 break;
         }
     });
-    evalState = 'new';
     openBrackets = false;
-
-    function isOperatorValid(exp) {
-        let invalid = ['(','*','/','+','-'];
-        if (invalid.includes(exp[exp.length-1]) || exp === '') {
-            return false;
-        }
-        return true;
-    }
-
-    function isBracketValid(bracket,exp) {
-        let invalid = [];
-        if (bracket === '(') {
-            invalid = ['0','1','2','3','4','5','6','7','8','9','.'];
-        } else {
-            if (openBrackets === false) {
-                return false;
-            }
-            invalid = ['(','*','/','+','-'];
-        }
-        if (invalid.includes(exp[exp.length-1])) {
-            return false;
-        }
-        return true;
-    }
 });
+
+function isOperatorValid(exp) {
+    let invalid = ['(','*','/','+','-'];
+    if (invalid.includes(exp[exp.length-1]) || exp === '') {
+        return false;
+    }
+    return true;
+}
+
+function isBracketValid(bracket,exp) {
+    let invalid = [];
+    if (bracket === '(') {
+        invalid = ['0','1','2','3','4','5','6','7','8','9','.'];
+    } else {
+        if (openBrackets === false) {
+            return false;
+        }
+        invalid = ['(','*','/','+','-'];
+    }
+    if (invalid.includes(exp[exp.length-1])) {
+        return false;
+    }
+    return true;
+}
+
+function changeFontSize() {
+    $('#expression').css('font-size', $('#expressionDiv').height() * 0.8);
+    $('#result').css('font-size', $('#resultDiv').height() * 0.8);
+    $('button').css('font-size', $('button').height() * 0.8);
+}
+
+function setHeight() {
+    let resultHeight = $('#resultDiv').height();
+    let expressionHeight = $('#expressionDiv').height();
+    $('#resultDiv').height(resultHeight);
+    $('#expressionDiv').height(expressionHeight);
+}
